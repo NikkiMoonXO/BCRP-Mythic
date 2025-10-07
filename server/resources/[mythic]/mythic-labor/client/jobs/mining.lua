6baeaa719f6dcee38cb77d266ebd778f8b517b00
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-07 14:07:01 +0100

    Mining Job Fix

--- server/resources/[mythic]/mythic-labor/client/jobs/mining.lua
+++ server/resources/[mythic]/mythic-labor/client/jobs/mining.lua
@@ -1,397 +1,330 @@
+local _POLYID = "mining-zone"
 local _joiner = nil
 local _working = false
+local _state = 0
 local _blips = {}
 local eventHandlers = {}
 
+local _objs = {}
 local _nodes = nil
 
 local _sellers = {
-  {
-    coords = vector3(-619.496, -225.697, 37.057),
-    heading = 130.523,
-    model = `a_f_y_clubcust_02`,
-  },
-  {
-    coords = vector3(232.280, 373.679, 105.142),
-    heading = 162.529,
-    model = `csb_popov`,
-  },
+	{
+		coords = vector3(-623.161, -237.151, 37.170),
+		heading = 73.721,
+		model = `a_f_y_clubcust_02`,
+	},
+	-- {
+	-- 	coords = vector3(244.655, 374.674, 105.738),
+	-- 	heading = 162.529,
+	-- 	model = `csb_popov`,
+	-- },
 }
 
-local miningSoundThread = false
+local function SpawnOres()
+	if _nodes then
+		for k, v in ipairs(_nodes) do
+			RequestModel(v.ore.object)
+			while not HasModelLoaded(v.ore.object) do
+				Citizen.Wait(5)
+			end
 
-AddEventHandler("Labor:Client:Setup", function()
-  for k, v in ipairs(_sellers) do
-    PedInteraction:Add(string.format("GemSeller%s", k), v.model, v.coords, v.heading, 25.0, { 
-      {
-        icon = "sack-dollar",
-        text = "Sell Diamonds",
-        event = "Mining:Client:SellGem",
-        data = "diamond",
-        item = "diamond",
-        rep = { id = "Mining", level = 5 },
-      },
-      {
-        icon = "sack-dollar",
-        text = "Sell Emeralds",
-        event = "Mining:Client:SellGem",
-        data = "emerald",
-        item = "emerald",
-        rep = { id = "Mining", level = 5 },
-      },
-      {
-        icon = "sack-dollar",
-        text = "Sell Sapphire",
-        event = "Mining:Client:SellGem",
-        data = "sapphire",
-        item = "sapphire",
-        rep = { id = "Mining", level = 4 },
-      },
-      {
-        icon = "sack-dollar",
-        text = "Sell Ruby",
-        event = "Mining:Client:SellGem",
-        data = "ruby",
-        item = "ruby",
-        rep = { id = "Mining", level = 3 },
-      },
-      {
-        icon = "sack-dollar",
-        text = "Sell Amethyst",
-        event = "Mining:Client:SellGem",
-        data = "amethyst",
-        item = "amethyst",
-        rep = { id = "Mining", level = 2 },
-      },
-      {
-        icon = "sack-dollar",
-        text = "Sell Citrine",
-        event = "Mining:Client:SellGem",
-        data = "citrine",
-        item = "citrine",
-        rep = { id = "Mining", level = 1 },
-      },
-      {
-        icon = "sack-dollar",
-        text = "Sell Opal",
-        event = "Mining:Client:SellGem",
-        data = "opal",
-        item = "opal",
-        rep = { id = "Mining", level = 1 },
-      },
-    }, 'gem')
-  end
+			local o = CreateObject(v.ore.object, v.location.x, v.location.y, v.location.z - 1.4, false, true, false)
+			PlaceObjectOnGroundProperly(o)
+			FreezeEntityPosition(o, true)
+			Targeting:AddEntity(o, "hill-rockslide", {
+				{
+					text = string.format("Mine %s", v.ore.label),
+					icon = "trowel",
+					event = string.format("Mining:Client:%s:Action", _joiner),
+					data = v,
+					minDist = 3.0,
+				},
+			}, 3.0)
+			local b = Blips:Add(string.format("MiningNode-%s", k), "Mining Node", v.location, 594, 0, 0.8)
 
-  PedInteraction:Add("MiningJob", `s_m_y_construct_02`, vector3(2741.874, 2791.691, 34.214), 155.045, 25.0, {
-    {
-      icon = "hand-holding-dollar",
-      text = "Sell Crushed Stone ($3/per)",
-      event = "Mining:Client:SellStone",
-    },
-    {
-      icon = "helmet-safety",
-      text = "Start Work",
-      event = "Mining:Client:StartJob",
-      tempjob = "Mining",
-      isEnabled = function()
-        return not _working
-      end,
-    },
-    {
-      icon = "dollar-sign",
-      text = "Buy Pickaxe ($250)",
-      event = "Mining:Client:PuchaseAxe",
-      tempjob = "Mining",
-    },
-    {
-      icon = "handshake-angle",
-      text = "Turn In Ore",
-      event = "Mining:Client:TurnIn",
-      tempjob = "Mining",
-      isEnabled = function()
-        return _working
-      end,
-    },
-  }, 'helmet-safety')
-
-  Callbacks:RegisterClientCallback("Mining:DoTheThingBrother", function(item, cb)
-    local lastCooldown = GetGameTimer()
-
-    MiningRockSound()
-
-    Progress:ProgressWithTickEvent({
-      name = "mining_action",
-      duration = math.random(13, 35) * 1000,
-      label = "Mining Ore",
-      useWhileDead = false,
-      canCancel = true,
-      vehicle = false,
-      controlDisables = {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableCombat = true,
-      },
-      animation = {
-        animDict = "melee@large_wpn@streamed_core",
-        anim = "ground_attack_on_spot",
-        flags = 1
-      },
-      prop = {
-        model = "prop_tool_pickaxe",
-        bone = 57005,
-        coords = { x = 0.1, y = -0.1, z = -0.02 },
-        rotation = { x = 80.0, y = 0.00, z = 170.0 },
-      }
-    }, function(pickaxeNetId)
-      if NetworkDoesNetworkIdExist(pickaxeNetId) and NetworkDoesEntityExistWithNetworkId(pickaxeNetId) then
-        local pickaxe = NetworkGetEntityFromNetworkId(pickaxeNetId)
-
-        local animTime = round(
-          GetEntityAnimCurrentTime(LocalPlayer.state.ped, "melee@large_wpn@streamed_core", "ground_attack_on_spot"), 2)
-
-        if animTime == 0.35 then
-          if GetGameTimer() - lastCooldown >= 150 then
-            PlayParticleFx("core", "ent_dst_rocks", 50, 0.8, 0.0, 0.0, 1.0, pickaxe)
-
-            Sounds.Play:Location(GetEntityCoords(pickaxe), 8.0, "dirt.ogg", 0.25)
-
-            lastCooldown = GetGameTimer()
-          end
-        end
-      end
-    end, function(cancelled)
-      miningSoundThread = false
-      cb(not cancelled)
-    end)
-  end)
-end)
-
-
-local function putOresOnRock(rockIndex, rock)
-  local ores = {}
-
-  local rockModel = GetEntityModel(rock)
-
-  for _, oreOffset in ipairs(Mining.Ores[rockModel]) do
-    local randomOre = Utils:WeightedRandom({
-      {
-        15,
-        {
-          model = "prop_goldore_01",
-
-          item = "goldore"
-        }
-      },
-      {
-        1,
-        {
-          model = "prop_rubyore_01",
-
-          item = "ruby",
-
-          count = 1
-        }
-      },
-      {
-        700,
-        {
-          model = "prop_ironore_01",
-
-          item = "ironore"
-        }
-      },
-      {
-        275,
-        {
-          model = "prop_ironore_01",
-
-          item = "chromite"
-        }
-      },
-      {
-        275,
-        {
-          model = "prop_ironore_01",
-
-          item = "silverore"
-        }
-      },
-    })
-
-    local randomOreModel = GetHashKey(randomOre.model)
-
-    while not HasModelLoaded(randomOreModel) do
-      Wait(0)
-
-      RequestModel(randomOreModel)
-    end
-
-    local randomStonePosition = GetOffsetFromEntityInWorldCoords(rock, oreOffset)
-
-    local oreHandle = CreateObject(randomOreModel, randomStonePosition, false)
-
-    SetEntityHeading(oreHandle, math.random(120) + 0.1)
-
-    if IsEntityTouchingEntity(oreHandle, rock) then
-      FreezeEntityPosition(oreHandle, true)
-
-      table.insert(ores, oreHandle)
-
-	  Targeting:AddEntity(oreHandle, "hill-rockslide", {
-		{
-			icon = "fas fa-circle-dot",
-			text = "Mine Ore",
-			event = "Mining:Client:MineOre",
-			data = {
-				index = rockIndex,
-				ore = randomOre
-			  }
-		},
-	})
-    else
-      DeleteEntity(oreHandle)
-    end
-
-    SetModelAsNoLongerNeeded(randomOreModel)
-  end
-
-  return ores
+			table.insert(_objs, {
+				ent = o,
+				data = v,
+				blip = b,
+				blipId = string.format("MiningNode-%s", k),
+			})
+		end
+	end
 end
 
+local function DeleteNode(location)
+	if _objs and #_objs > 0 then
+		for k, v in ipairs(_objs) do
+			if
+				vector3(v.data.location.x, v.data.location.y, v.data.location.z)
+				== vector3(location.x, location.y, location.z)
+			then
+				Targeting:RemoveEntity(v.ent)
+				DeleteObject(v.ent)
+				Blips:Remove(v.blipId)
 
-RegisterNetEvent("Mining:Client:MineOre", function(data, oreData)
-  if Inventory.Check.Player:HasItem("pickaxe", 1) then
-    Callbacks:ServerCallback('Mining:Server:MineOre', oreData, function(finishedMining)
-      if finishedMining then
-        DeleteEntity(data.entity)
-      end
-      miningSoundThread = false
-    end)
-  else
-    Notification:Error("You Need A Pickaxe To Mine This.")
-  end
+				table.remove(_objs, k)
+				break
+			end
+		end
+
+		for k, v in ipairs(_nodes) do
+			if v.location == location then
+				table.remove(_nodes, k)
+				break
+			end
+		end
+	end
+end
+
+local function DespawnOres()
+	if _objs and #_objs > 0 then
+		for i = #_objs, 0, -1 do
+			local v = _objs[i]
+
+			if v then
+				Targeting:RemoveEntity(v.ent)
+				DeleteObject(v.ent)
+				Blips:Remove(v.blipId)
+			end
+
+			table.remove(_objs, i)
+		end
+	end
+end
+
+AddEventHandler("Labor:Client:Setup", function()
+	Polyzone.Create:Circle(_POLYID, vector3(2885.78, 2803.96, 54.76), 350.0, {
+		name = _POLYID,
+		useZ = false,
+		--debugPoly=true
+	})
+
+	for k, v in ipairs(_sellers) do
+		PedInteraction:Add(string.format("GemSeller%s", k), v.model, v.coords, v.heading, 25.0, {
+			{
+				icon = "sack-dollar",
+				text = "Sell Diamonds",
+				event = "Mining:Client:SellGem",
+				data = "diamond",
+				item = "diamond",
+				rep = { id = "Mining", level = 5 },
+			},
+			{
+				icon = "sack-dollar",
+				text = "Sell Emeralds",
+				event = "Mining:Client:SellGem",
+				data = "emerald",
+				item = "emerald",
+				rep = { id = "Mining", level = 5 },
+			},
+			{
+				icon = "sack-dollar",
+				text = "Sell Sapphire",
+				event = "Mining:Client:SellGem",
+				data = "sapphire",
+				item = "sapphire",
+				rep = { id = "Mining", level = 4 },
+			},
+			{
+				icon = "sack-dollar",
+				text = "Sell Ruby",
+				event = "Mining:Client:SellGem",
+				data = "ruby",
+				item = "ruby",
+				rep = { id = "Mining", level = 3 },
+			},
+			{
+				icon = "sack-dollar",
+				text = "Sell Amethyst",
+				event = "Mining:Client:SellGem",
+				data = "amethyst",
+				item = "amethyst",
+				rep = { id = "Mining", level = 2 },
+			},
+			{
+				icon = "sack-dollar",
+				text = "Sell Citrine",
+				event = "Mining:Client:SellGem",
+				data = "citrine",
+				item = "citrine",
+				rep = { id = "Mining", level = 1 },
+			},
+			{
+				icon = "sack-dollar",
+				text = "Sell Opal",
+				event = "Mining:Client:SellGem",
+				data = "opal",
+				item = "opal",
+				rep = { id = "Mining", level = 1 },
+			},
+		}, "gem")
+	end
+
+	PedInteraction:Add("MiningJob", `s_m_y_construct_02`, vector3(2741.874, 2791.691, 34.214), 155.045, 25.0, {
+		{
+			icon = "money-bill-trend-up",
+			text = "Sell Crushed Stone ($3/per)",
+			event = "Mining:Client:SellStone",
+		},
+		{
+			icon = "helmet-safety",
+			text = "Start Work",
+			event = "Mining:Client:StartJob",
+			tempjob = "Mining",
+			isEnabled = function()
+				return not _working
+			end,
+		},
+	}, "helmet-safety")
 end)
 
+local attempt = 0
 RegisterNetEvent("Mining:Client:OnDuty", function(joiner, time)
-  _joiner = joiner
-  DeleteWaypoint()
-  SetNewWaypoint(2741.874, 2791.691)
+	_joiner = joiner
+	DeleteWaypoint()
+	SetNewWaypoint(2741.874, 2791.691)
+	_node = 0
 
-  eventHandlers["startup"] = RegisterNetEvent(string.format("Mining:Client:%s:Startup", joiner), function(nodes)
-    if _nodes ~= nil then return end
+	eventHandlers["startup"] = RegisterNetEvent(string.format("Mining:Client:%s:Startup", joiner), function(nodes)
+		if _nodes ~= nil then
+			return
+		end
+		_working = true
+		_nodes = nodes
+		_state = 1
 
-    _working = true
-    _nodes = nodes
+		SpawnOres()
+	end)
 
-    local rockModels = {
-      GetHashKey("prop_rock_3_j"),
-      GetHashKey("prop_rock_4_a"),
-      GetHashKey("prop_rock_1_f"),
-      GetHashKey("prop_rock_3_j"),
-      GetHashKey("prop_rock_1_i")
-    }
+	eventHandlers["actions"] = RegisterNetEvent(string.format("Mining:Client:%s:Action", joiner), function(ent, data)
+		attempt = 0
+		if data.luck <= 10 then
+			Citizen.CreateThread(function()
+				local p = promise.new()
+				while attempt < 3 do
+					local p2 = promise.new()
+					Minigame.Play:RoundSkillbar((data?.ore?.factor or 0.75) * (data?.ore?.scale or 1.15), (data?.ore?.size or 6) - attempt, {
+						onSuccess = function()
+							Citizen.Wait(400)
+							attempt += 1
+							p2:resolve(true)
 
-    for k, v in pairs(_nodes) do
-      local rockModel = rockModels[math.random(#rockModels)]
+							if attempt >= 3 then
+								p:resolve(true)
+							end
+						end,
+						onFail = function()
+							data.failed = true
+							p:resolve(false)
+							p2:resolve(true)
+							attempt = 3
+						end,
+					}, {
+						useWhileDead = false,
+						vehicle = false,
+						controlDisables = {
+							disableMovement = true,
+							disableCarMovement = true,
+							disableMouse = false,
+							disableCombat = true,
+						},
+						animation = {
+							anim = "mining",
+						},
+					})
 
-      while not HasModelLoaded(rockModel) do
-        RequestModel(rockModel)
+					Citizen.Await(p2)
+				end
+	
+				local r = Citizen.Await(p)
+				Callbacks:ServerCallback("Mining:Server:MineNode", data)
+			end)
+		else
+			Progress:Progress({
+				name = "mining_action",
+				duration = math.random(40, 55) * 1000,
+				label = "Mining",
+				useWhileDead = false,
+				canCancel = true,
+				vehicle = false,
+				animation = {
+					anim = "mining",
+				},
+				controlDisables = {
+					disableMovement = true,
+					disableCarMovement = true,
+					disableCombat = true,
+				},
+			}, function(cancelled)
+				if not cancelled then
+					Callbacks:ServerCallback("Mining:Server:MineNode", data)
+				end
+			end)
+		end
 
-        Wait(0)
-      end
+	end)
 
-      local rockHandle = CreateObject(rockModel, v.coords - vector3(0.0, 0.0, 1.0))
+	eventHandlers["remove-node"] = RegisterNetEvent(
+		string.format("Mining:Client:%s:RemoveNode", joiner),
+		function(location)
+			DeleteNode(location)
+		end
+	)
 
-      PlaceObjectOnGroundProperly(rockHandle)
+	eventHandlers["enter-poly"] = AddEventHandler("Polyzone:Enter", function(id, testedPoint, insideZones, data)
+		if id ~= _POLYID or not _nodes or #_nodes == 0 then
+			return
+		end
 
-      FreezeEntityPosition(rockHandle, true)
+		_inPoly = true
+		if _state == 1 then
+			SpawnOres()
+		end
+	end)
 
-      v.handle = rockHandle
-
-      local ores = putOresOnRock(v.id, rockHandle)
-
-      v.ores = ores
-
-      Blips:Add(string.format("MiningNode-%s", v.id), "Mining Node", v.coords, 679, 18, 0.8)
-
-      SetModelAsNoLongerNeeded(rockModel)
-    end
-  end)
-
-  eventHandlers["actions"] = RegisterNetEvent(string.format("Mining:Client:%s:Action", joiner), function(data)
-    for k, v in pairs(_nodes) do
-      if v.id == data then
-        Blips:Remove(string.format("MiningNode-%s", v.id))
-        _nodes[k] = nil
-
-        if v.handle and DoesEntityExist(v.handle) then
-          DeleteEntity(v.handle)
-        end
-
-        if v.ores then
-          for _, ore in ipairs(v.ores) do
-            if ore and DoesEntityExist(ore) then
-              DeleteEntity(ore)
-            end
-          end
-        end
-
-        break
-      end
-    end
-  end)
+	eventHandlers["exit-poly"] = AddEventHandler("Polyzone:Exit", function(id, testedPoint, insideZones, data)
+		if id == _POLYID then
+			_inPoly = false
+			DespawnOres()
+		end
+	end)
 end)
 
 AddEventHandler("Mining:Client:SellStone", function()
-  Callbacks:ServerCallback('Mining:SellStone', {}) 
+	Callbacks:ServerCallback("Mining:SellStone", {})
 end)
 
 AddEventHandler("Mining:Client:PuchaseAxe", function()
-  Callbacks:ServerCallback('Mining:PurchasePickaxe', {})
+	Callbacks:ServerCallback("Mining:PurchasePickaxe", {})
 end)
 
 AddEventHandler("Mining:Client:TurnIn", function()
-  Callbacks:ServerCallback('Mining:TurnIn', _joiner)
+	Callbacks:ServerCallback("Mining:TurnIn", _joiner)
 end)
 
 AddEventHandler("Mining:Client:SellGem", function(entity, data)
-  Callbacks:ServerCallback("Mining:SellGem", data)
+	Callbacks:ServerCallback("Mining:SellGem", data)
 end)
 
 AddEventHandler("Mining:Client:StartJob", function()
-  Callbacks:ServerCallback('Mining:StartJob', _joiner, function(state)
-    if not state then
-      Notification:Error("Unable To Start Job") 
-    end
-  end)
+	Callbacks:ServerCallback("Mining:StartJob", _joiner, function(state)
+		if not state then
+			Notification:Error("Unable To Start Job")
+		end
+	end)
 end)
 
 RegisterNetEvent("Mining:Client:OffDuty", function(time)
-  for k, v in pairs(eventHandlers) do
-    RemoveEventHandler(v)
-  end
+	for k, v in pairs(eventHandlers) do
+		RemoveEventHandler(v)
+	end
 
-  if _nodes ~= nil then
-    for k, v in pairs(_nodes) do
-      Blips:Remove(string.format("MiningNode-%s", v.id)) 
-    end
-  end
+	DespawnOres()
 
-  eventHandlers = {}
-  _joiner = nil
-  _working = false
-  _nodes = nil
+	eventHandlers = {}
+	_joiner = nil
+	_working = false
+	_nodes = nil
 end)
-
-function MiningRockSound()
-  if miningSoundThread then return end
-  miningSoundThread = true
-  CreateThread(function()
-    local soundTimeout = 2100
-    while miningSoundThread do
-      Wait(soundTimeout)
-      if not miningSoundThread then break end
-      Sounds.Do.Play:One('mining.ogg', 0.5)
-    end
-  end)
-end
