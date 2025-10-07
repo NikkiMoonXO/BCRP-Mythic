c099e47a6043ffb1b3da12028a7637559afd3e42
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-07 14:31:51 +0100

    Artifacts

--- artifacts/citizen/scripting/v8/main.js
+++ artifacts/citizen/scripting/v8/main.js
@@ -8,6 +8,7 @@ const EXT_LOCALFUNCREF = 11;
 	let boundaryIdx = 1;
 	let lastBoundaryStart = null;
 	const isDuplicityVersion = IsDuplicityVersion();
+	const currentResourceName = GetCurrentResourceName();
 
 	// temp
 	global.FormatStackTrace = function (args, argLength) {
@@ -68,6 +69,17 @@ const EXT_LOCALFUNCREF = 11;
 		return Citizen.canonicalizeRef(ref);
 	};
 
+	/**
+	 * @param {Function} refFunction
+	 * @returns {string|null} the function reference, or null if the refFunction that was passed wasn't a function
+	 */
+	Citizen.getRefFunction = (refFunction) => {
+		if (typeof refFunction !== "function") {
+			return null;
+		}
+		return Citizen.makeRefFunction(refFunction);
+	}
+
 	function refFunctionPacker(refFunction) {
 		const ref = Citizen.makeRefFunction(refFunction);
 
@@ -76,13 +88,30 @@ const EXT_LOCALFUNCREF = 11;
 
 	function refFunctionUnpacker(refSerialized) {
 		const fnRef = Citizen.makeFunctionReference(refSerialized);
+		const invoker = GetInvokingResource();
 
 		return function (...args) {
 			return runWithBoundaryEnd(() => {
-				const retvals = unpack(fnRef(pack(args)));
+				let retvals = null;
+				try {
+					retvals = unpack(fnRef(pack(args)));
+				} catch (e) {
+				}
 
 				if (retvals === null) {
-					throw new Error('Error in nested ref call.');
+					let errorMessage = `Error in nested ref call for ${currentResourceName}. `
+					// invoker can be null, we don't want to give an even worse
+					// error by erroring here :P
+					if (invoker) {
+						errorMessage += `${currentResourceName} tried to call a function reference in ${invoker} but the reference wasn't valid. `
+						if (GetResourceState(invoker) !== "started") {
+							errorMessage += `And ${invoker} isn't started, was the resource restarted mid call?`
+						} else {
+							errorMessage += `(did ${invoker} restart recently?)`
+						}
+					}
+
+					throw new Error(errorMessage);
 				}
 
 				switch (retvals.length) {
@@ -516,10 +545,15 @@ const EXT_LOCALFUNCREF = 11;
 	const exportKey = isDuplicityVersion ? 'server_export' : 'export';
 	const eventType = isDuplicityVersion ? 'Server' : 'Client';
 
-	const getExportEventName = (resource, name) => `__cfx_export_${resource}_${name}`;
+	const getExportEventName = (resource, name) => {
+		if(resource == "txAdmin") {
+			resource = "monitor";
+		}
+		return `__cfx_export_${resource}_${name}`;
+	}
 
 	on(`on${eventType}ResourceStart`, (resource) => {
-		if (resource === GetCurrentResourceName()) {
+		if (resource === currentResourceName) {
 			const numMetaData = GetNumResourceMetadata(resource, exportKey) || 0;
 
 			for (let i = 0; i < numMetaData; i++) {
@@ -582,7 +616,7 @@ const EXT_LOCALFUNCREF = 11;
 
 				const [exportName, func] = args;
 
-				on(getExportEventName(GetCurrentResourceName(), exportName), (setCB) => {
+				on(getExportEventName(currentResourceName, exportName), (setCB) => {
 					setCB(func);
 				});
 			},
